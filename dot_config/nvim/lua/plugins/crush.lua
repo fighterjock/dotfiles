33733123032b5be8a0488bb46d2b56c2c31ae670
fighterjock <git@rudresha.com>
2026-09-04 05:48:42 -0700

    Keep Crush terminals floating; glow stays a vertical pane

--- dot_config/nvim/lua/plugins/crush.lua
+++ dot_config/nvim/lua/plugins/crush.lua
@@ -8,8 +8,5 @@ return {
     { "<leader>cr", mode = "v", desc = "Crush: run on selection" },
     { "<leader>cf", desc = "Crush: send file to Crush" },
   },
-  opts = {
-    direction = "vertical",
-    shell_direction = "vertical",
-  },
+  opts = {},
 }
